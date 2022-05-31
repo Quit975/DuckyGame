d75a8ba36ac4969b0a7a02301cd1c59cb192b6dd
Ania000 <82574004+Ania000@users.noreply.github.com>
2022-05-31 16:15:22 +0200

    Frog teleports away from player (#58) _issue #47
    Resolves issue #47 
    frog now checks player position before teleporting, so it doesn't count double
    KeepPlayerInBounds() function improved, now only sets the position of the duckySprite
    duckyMath library added, includes functions calculating vector length and dividing vectors

--- src/Entity.h
+++ src/Entity.h
@@ -11,6 +11,7 @@ public:
 	virtual void Draw(sf::RenderWindow& window) = 0;
 	virtual void Update(const float dt) = 0;
 	virtual sf::FloatRect GetBounds() = 0;
+	virtual sf::Vector2f GetLocation() = 0;
 	
 };
 
--- src/Player.h
+++ src/Player.h
@@ -9,19 +9,21 @@ public:
 	virtual void Update(const float dt) override;
 	virtual void Draw(sf::RenderWindow& window) override;
 	virtual sf::FloatRect GetBounds() override;
+	virtual sf::Vector2f GetLocation() override;
 	void Quack();
 	bool Hit();
 	void ResetHit();
 	void KeepPlayerInBounds();
+	
 
 private:
 	sf::Texture duckyTexture;
 	sf::Sprite duckySprite;
 	sf::SoundBuffer quackBuffer;
 	sf::Sound quackSound;
-
 	sf::CircleShape collisionShape;
-
+	
+	float duckyRadius = 45.f; //desired ducky radius, this can be temporary, but I haven't yet figured out how to get the right size from sprite or texture
 	bool gequacked = false;
 	const float speed = 220.f;
 
