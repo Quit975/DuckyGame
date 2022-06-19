279a8a1fdac531894a74052ed55ca25eb21452ed
Ania000 <annasidor00@gmail.com>
2022-06-19 14:56:58 +0200

    smart pointers instead of raw

--- src/Entity.h
+++ src/Entity.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
+#include <memory>
+#include "EntityComponent.h"	//to avoid "use of undefined type" error, otherwise occuring when using unique_ptr instead of raw
 
-class EntityComponent;
+
 
 extern const int WindowWidth;
 extern const int WindowHeight;
@@ -10,7 +12,7 @@ extern const int WindowHeight;
 class Entity
 {
 private:
-	std::unordered_map<const char*,EntityComponent*> Components;
+	std::unordered_map<const char*,std::unique_ptr<EntityComponent>> Components;
 
 public:
 	Entity() {};
@@ -23,8 +25,8 @@ public:
 	template <class T>
 	T* CreateComponent(const char* componentName)
 	{
-		Components.emplace(componentName, new T());
-		return static_cast<T*>(Components.at(componentName));
+		Components.emplace(componentName, std::make_unique<T>());
+		return static_cast<T*>(Components.at(componentName).get());
 	}
 };
 
